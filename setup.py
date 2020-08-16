865022a33b1e32cfa677e3cb09952b2f0bbc2687
oceaniasam <oceaniasam>
2020-08-16 21:50:01 +0530

    Added download URL in the setup file

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name = 'suresh',
+    packages = ['suresh'],
+    version = '1.0.0',  # Ideally should be same as your GitHub release tag varsion
+    description = "It's used to simplify system operation",
+    author = 'Suresh Arunachalam T',
+    author_email = 'tsuresh97@gmail.com',
+    url = 'https://github.com/tsuresh97/suresh',
+    download_url = 'http://github.com/tsuresh97/suresh/archive/1.0.0.tar.gz',
+    keywords = ["It's used to simplify system operation"],
+    classifiers = [],
+)
